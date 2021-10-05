a8c6dfb172dfb7c96b690736eb92a6c1cb4eb4c1
Sauterey Boris <boris.sauterey@ens.fr>
2021-10-05 14:22:01 +0200

    Readme
    - Added a real readme
    - Removed the corresponding comments from the main code

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # C_N_ratio
- 
+
+/// To run for compilation: "gcc -o code Shared_Code_lat_input.c -fopenmp"
+/// After compilation, this version of the code compute the eco-evolutionary dynamics
+/// of the plankton community in one location of the North-Atlantic transect, and takes
+/// the latitude of that location as argument (i.e., type "./code lat"). It can be
+/// modified to do the same for multiple locations (i.e., by setting Spaceres > 0).
--- Shared_Code_lat_input.c
+++ Shared_Code_lat_input.c
@@ -1,17 +1,8 @@
 //
-//  Main.c
-//  Code_Trait_diff
+//  Shared_Code_lat_input.c
 //
-//  Created by boris on 15/01/2014.
+//  Created by boris Sauterey on 05/10/2021.
 //  Copyright (c) 2014 boris. All rights reserved.
-//
-
-
-/// To run for compilation: "gcc -o code Shared_Code_lat_input.c -fopenmp"
-/// After compilation, this version of the code compute the eco-evolutionary dynamics
-/// of the plankton community in one location of the North-Atlantic transect, and takes
-/// the latitude of that location as argument (i.e., type "./code lat"). It can be
-/// modified to do the same for multiple locations (i.e., by setting Spaceres > 0).
 
 #include <stdio.h>
 #include <math.h>
